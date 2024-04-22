0ae5deaac37e1e4037248e79570967488591cd3a
Fabian Speh <132446305+FabianSpeh@users.noreply.github.com>
2024-04-22 18:36:47 +0200

    added basic calculation functions add, subtract, multiply and divide
    added basic layout of calculator with html anc css

---
+++ index.js
@@ -0,0 +1,38 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+const add = (number1, number2) => {
+  return number1 + number2;
+};
+
+const subtract = (number1, number2) => {
+  return number1 - number2;
+};
+
+const multiply = (number1, number2) => {
+  return number1 * number2;
+};
+
+const divide = (number1, number2) => {
+  return number1 / number2;
+};
+
+const operate = (number1, number2, operator) => {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(number1, number2);
+      break;
+    case "-":
+      result = subtract(number1, number2);
+      break;
+    case "*":
+      result = multiply(number1, number2);
+      break;
+    case "/":
+      result = divide(number1, number2);
+      break;
+  }
+  return result;
+};
