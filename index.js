9b7e38dd947fdfdc8ede31a2a8018d8798e46cbb
Fabian Speh <132446305+FabianSpeh@users.noreply.github.com>
2024-04-23 00:14:56 +0200

    added comments to functions and variables

--- index.js
+++ index.js
@@ -4,16 +4,32 @@
 // implement factorial button
 
 let firstNumber = null;
-let secondNumber = null;
-let operator = "";
-let firstAction = true;
-let operatorActive = false;
 
+let secondNumber = null;
+
+let operator = "";
+
+// specifies whether the first Action in a calculation has been taken (first number has been typed in)
+let firstAction = true;
+// specifies whether an operator has been selected
+let operatorActive = false;
+// holds the current selected operator
+let currentOperator = null;
+
+// contains all number Buttons
 const numberButtons = document.querySelectorAll(".number");
+// contains the clear button(C)
 const clearButton = document.querySelector(".clear");
+// contains all operator buttons (+,-,*,/)
 const operatorButtons = document.querySelectorAll(".operator");
+// contains the equals button (=)
 const equalsButton = document.querySelector(".equals");
+// contains the display field
 const input = document.querySelector(".input");
+// contains the zero button
+const zeroButton = document.querySelector(".zero");
+// contains the back button
+const undoButton = document.querySelector(".undo");
 
 equalsButton.addEventListener("click", function () {
   if (operatorActive) {
@@ -21,12 +37,27 @@ equalsButton.addEventListener("click", function () {
     input.textContent =
       Math.round(operate(firstNumber, secondNumber, operator) * 1000000000000) /
       1000000000000;
+    currentOperator.classList.remove("active");
     operatorActive = false;
   }
 });
 
+undoButton.addEventListener("click", function () {
+  input.textContent = input.textContent.slice(0, -1);
+});
+
+// adds functionality to the operator buttons
 operatorButtons.forEach((button) => {
   button.addEventListener("click", function () {
+    for (let i = 0; i < operatorButtons.length; i++) {
+      operatorButtons[i].classList.remove("active");
+    }
+    if (!firstAction) {
+      // adds an active state to the active operator button
+      button.classList.add("active");
+    }
+
+    currentOperator = button;
     firstNumber = Number(input.textContent);
     operator = button.textContent;
     operatorActive = true;
@@ -45,6 +76,12 @@ numberButtons.forEach((button) => {
     }
     console.log(firstAction);
   });
+});
+
+zeroButton.addEventListener("click", function () {
+  if (!firstAction && input.textContent.length <= 13) {
+    input.textContent += zeroButton.textContent;
+  }
 });
 
 clearButton.addEventListener("click", function () {
